ef77afd82c76adb7d5b0cab36fee872071a256b0
pablofanjul88 <108837903+pablofanjul88@users.noreply.github.com>
2023-04-29 17:19:25 -0300

    rutas

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping
+@CrossOrigin(origins = {"https://pmffrontend.web.app/","http://localhost:4200"})
 public class PersonaController {
 
     @Autowired

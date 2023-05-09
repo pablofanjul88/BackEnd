ac09f74063fbc8322fd80540d0683add1a1dad76
pablofanjul88 <108837903+pablofanjul88@users.noreply.github.com>
2023-05-09 14:20:58 -0300

    agregando request mapping en personaController

--- src/main/java/com/portfolio/mgb/Controller/PersonaController.java
+++ src/main/java/com/portfolio/mgb/Controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/personas")
 @CrossOrigin(origins = "https://pmffrontend.web.app")
 public class PersonaController {
 
